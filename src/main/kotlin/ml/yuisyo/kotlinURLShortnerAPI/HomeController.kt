b57b41e0506f0e7f7dd198916d9a3d8c27294422
Yuto Fujimura <yuto101024@gmail.com>
2018-09-14 08:35:23 +0900

    modified:   src/main/kotlin/ml/yuisyo/kotlinURLShortnerAPI/HomeController.kt

--- src/main/kotlin/ml/yuisyo/kotlinURLShortnerAPI/HomeController.kt
+++ src/main/kotlin/ml/yuisyo/kotlinURLShortnerAPI/HomeController.kt
@@ -1,7 +1,13 @@
 package ml.yuisyo.kotlinURLShortnerAPI
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import java.io.PrintWriter
+import java.lang.Runtime.*
 import java.util.*
 
 @RestController
@@ -10,12 +16,21 @@ class HomeController {
     fun index(): String = "<h1>URL shortener"
 
     @GetMapping("/json")
-    fun json(): Map<String, String> = mapOf("url" to "https://yuisyo.ml/" + rndText())
+    fun json(@RequestParam("url") url: String): Map<String, String> {
+        val text = rndText()
+        val file = FileWriter("/home/yuto/test.conf.d/$text.conf")
+        val pw = PrintWriter(BufferedWriter(file))
+        pw.println()
+        pw.println("rewrite ^/$text(.*)\$ $url permanent;")
+        pw.close()
+        getRuntime().exec("sudo systemctl restart nginx.service")
+        return mapOf("original url" to url, "url" to "http://localhost:8080/" + text)
+    }
 
-    private fun rndText(): String{
+    private fun rndText(): String {
         val charSet = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         val rand = Random()
-        var text:String = ""
+        var text = ""
         for (i in 0 .. 5) {
             text += charSet.get(rand.nextInt(charSet.length))
         }
