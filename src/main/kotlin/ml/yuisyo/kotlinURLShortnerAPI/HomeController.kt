214ef7e8aeb34b60a459aecb87ca5bb74cd5e8fd
Yuto Fujimura <yuto101024@gmail.com>
2018-11-08 19:16:36 +0900

    update Kotlin version to 1.3.0 modified:   build.gradle modified:   src/main/kotlin/ml/yuisyo/kotlinURLShortnerAPI/HomeController.kt

--- src/main/kotlin/ml/yuisyo/kotlinURLShortnerAPI/HomeController.kt
+++ src/main/kotlin/ml/yuisyo/kotlinURLShortnerAPI/HomeController.kt
@@ -13,7 +13,13 @@ import java.util.*
 @RestController
 class HomeController {
     @GetMapping("")
-    fun index(): String = "<h1>URL shortener"
+    fun index(): String = """
+        <h1>URL shortener</h1>
+        <form action="test" method="get">
+            <input type="text" value="url">
+            <input type="submit" value="send">
+        </form>
+        """.trimIndent()
 
     @GetMapping("/json")
     fun json(@RequestParam("url") url: String): Map<String, String> {
@@ -24,7 +30,19 @@ class HomeController {
         pw.println("rewrite ^/$text(.*)\$ $url permanent;")
         pw.close()
         getRuntime().exec("sudo systemctl restart nginx.service")
-        return mapOf("original url" to url, "url" to "http://localhost:8080/" + text)
+        return mapOf("original url" to url, "url" to "http://localhost/" + text)
+    }
+
+    @GetMapping("/test")
+    fun test(@RequestParam("url") url: String): String {
+        val text = rndText()
+        val file = FileWriter("/home/yuto/test.conf.d/$text.conf")
+        val pw = PrintWriter(BufferedWriter(file))
+        pw.println()
+        pw.println("rewrite ^/$text(.*)\$ $url permanent;")
+        pw.close()
+        getRuntime().exec("sudo systemctl restart nginx.service")
+        return "http://localhost/" + text
     }
 
     private fun rndText(): String {
